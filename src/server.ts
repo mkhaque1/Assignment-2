9606615969cf9594cf75e6c851d9378ef8f61ed1
mkhaque1 <mkhaque29@gmail.com>
2026-05-21 15:55:00 +0200

    fix projects initialization

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import { createServer, IncomingMessage } from 'node:http';
 import express from 'express';
+
+import type { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+const server = createServer((req: IncomingMessage, res) => {
+  app(req, res);
+});
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
